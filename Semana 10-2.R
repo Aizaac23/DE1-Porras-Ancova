a75e6841b715cbd08b95755db77820c2d64d0335
Aizaac23 <104815444+Aizaac23@users.noreply.github.com>
2025-06-13 09:54:41 -0500

    Update Semana 10-2.R

--- Semana 10-2.R
+++ Semana 10-2.R
@@ -34,4 +34,26 @@ tcxy<-sum(caso$ganancia)*sum(caso$inicial)/30
 sptoxy<-sum(caso$ganancia*caso$inicial)-tcxy
 sptraxy<-sum(tapply(caso$ganancia,caso$trat,sum)*tapply(caso$inicial,caso$trat,sum))/5-tcxy
 spbloxy<-sum(tapply(caso$ganancia,caso$Bloque,sum)*tapply(caso$inicial,caso$Bloque,sum))/6-tcxy
-scexy<-sptoxy-sptraxy-spbloxy
+scexy<-sptoxy-sptraxy-spbloxy # hasta acá es lo mismo que 10.2
+spxy<-c(spbloxy,sptraxy,scexy)
+
+SCySP<-cbind(anvay[,1],anvax[,2],spxy,anvay[,2])
+
+SCEaju<-SCySP[3,4]-SCySP[3,3]^2/SCySP[3,2]
+SCTraju<-sum(SCySP[2:3,4])-sum(SCySP[2:3,3])^2/sum(SCySP[2:3,2])
+SCDif<-SCTraju-SCEaju
+GLEAj<-anvay[3,1]-1
+CMEaj<-SCEaju/GLEAj
+CMTraj<-SCDif/anvax[2,1]
+
+ancova<-cbind(SCySP,c(0,0,SCEaju),c(0,0,GLAj),c(0,0,CMEaj))
+
+#Hipótesis para evaluar beta
+Fcal1<-(ancova[3,3]^2/ancova[3,2])/ancova[3,7]
+qf(0.05,1,19,lower.tail = F)
+pf(Fcal1,1,19,lower.tail = F)
+
+#Hipótesis para evaluar las medias ajustadas
+Fcal2<-CMTraj/CMEaj
+qf(0.05,5,19,lower.tail = F)
+pf(Fcal2,5,19,lower.tail = F)
